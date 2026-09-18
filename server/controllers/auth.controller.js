9d5561911fe217952d6922f16be1df665cf0cd2f
YYG <yyg27@outlook.com>
2026-09-19 01:32:06 +0300

    fix(server): replace bcrypt with bcryptjs

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import Subscription from "../models/subscription.model.js";
 
